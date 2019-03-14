c78ee4fa91bcdb09e8cdca01cf895431329d0789
Unknown <dskinner@uark.edu>
2019-03-14 16:02:12 -0500

    nothin

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const path = require('path');
+const router = express.Router();
+// const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// get html page
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  router.get('/about',function(req,res){
+    res.sendFile(path.join(__dirname+'/about.html'));
+  });
+  
+  router.get('/sitemap',function(req,res){
+    res.sendFile(path.join(__dirname+'/sitemap.html'));
+  });
+
+  //add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
