76b4f98c7ca208a68efbb889b15d6f86434b9e23
Unknown <dskinner@uark.edu>
2019-04-23 18:49:00 -0500

    more pathfinding

---
+++ ShortestPathTesting/ShortestPathTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ShortestPathTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string path = $"/Users/davidskinner/Documents/Repositories/CapstoneProject/ShortestPathTesting/ShortestPathTest/";
+            Bitmap bitmap = new Bitmap($"{path}map1.png");
+            Color clr = bitmap.GetPixel(50, 50);
+            Color clr2 = Color.FromName("Blue");
+            Color red = Color.Red;
+            bitmap.SetPixel(2, 2, clr2);
+            bitmap.SetPixel(3, 2, red);
+            Console.WriteLine($"This is the road: {bitmap.GetPixel(4,2)}");
+
+
+            // generate the final png
+            bitmap.Save($"{path}final.png", ImageFormat.Png);
+            
+        }
+    }
+}
