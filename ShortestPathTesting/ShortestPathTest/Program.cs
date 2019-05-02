95e40b1e5e1ba36bc7471d2cc892f70e350a139a
Unknown <dskinner@uark.edu>
2019-05-02 15:12:06 -0500

    added readme

--- ShortestPathTesting/ShortestPathTest/Program.cs
+++ ShortestPathTesting/ShortestPathTest/Program.cs
@@ -4,22 +4,30 @@ using System.Drawing.Imaging;
 
 namespace ShortestPathTest
 {
+
+    class Node
+    {
+        public int CurrentValue { get; set; }
+
+    }
     class MainClass
     {
+        // road color Color [A=255, R=234, G=217, B=176]
         public static void Main(string[] args)
         {
             string path = $"/Users/davidskinner/Documents/Repositories/CapstoneProject/ShortestPathTesting/ShortestPathTest/";
             Bitmap bitmap = new Bitmap($"{path}map1.png");
-            Color clr = bitmap.GetPixel(50, 50);
-            Color clr2 = Color.FromName("Blue");
+            Color clr2 = Color.Blue;
             Color red = Color.Red;
             bitmap.SetPixel(2, 2, clr2);
             bitmap.SetPixel(3, 2, red);
             Console.WriteLine($"This is the road: {bitmap.GetPixel(4,2)}");
 
 
-            // generate the final png
-            bitmap.Save($"{path}final.png", ImageFormat.Png);
+            //int[,] array = new int[bitmap.Height, bitmap.Width];
+
+        // generate the final png
+        bitmap.Save($"{path}final.png", ImageFormat.Png);
             
         }
     }
